18e471e8770504a1062bb5cb9f11065256b4e841
mac <2468751795@qq.com>
2021-08-03 11:05:21 +0800

    修改属性名称

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -64,33 +64,33 @@ open class TagListView: UIView {
         }
     }
     
-    @IBInspectable open dynamic var cornerRadius: CGFloat = 0 {
+    @IBInspectable open dynamic var tagCornerRadius: CGFloat = 0 {
         didSet {
             tagViews.forEach {
-                $0.cornerRadius = cornerRadius
+                $0.cornerRadius = tagCornerRadius
             }
         }
     }
-    @IBInspectable open dynamic var borderWidth: CGFloat = 0 {
+    @IBInspectable open dynamic var tagBorderWidth: CGFloat = 0 {
         didSet {
             tagViews.forEach {
-                $0.borderWidth = borderWidth
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var borderColor: UIColor? {
-        didSet {
-            tagViews.forEach {
-                $0.borderColor = borderColor
+                $0.borderWidth = tagBorderWidth
             }
         }
     }
     
-    @IBInspectable open dynamic var selectedBorderColor: UIColor? {
+    @IBInspectable open dynamic var tagBorderColor: UIColor? {
         didSet {
             tagViews.forEach {
-                $0.selectedBorderColor = selectedBorderColor
+                $0.borderColor = tagBorderColor
+            }
+        }
+    }
+    
+    @IBInspectable open dynamic var tagSelectedBorderColor: UIColor? {
+        didSet {
+            tagViews.forEach {
+                $0.selectedBorderColor = tagSelectedBorderColor
             }
         }
     }
@@ -296,7 +296,7 @@ open class TagListView: UIView {
             tagBackgroundView.frame.size = tagView.bounds.size
             tagView.frame.size.width = max(minWidth, tagView.frame.size.width)
             tagBackgroundView.layer.shadowColor = shadowColor.cgColor
-            tagBackgroundView.layer.shadowPath = UIBezierPath(roundedRect: tagBackgroundView.bounds, cornerRadius: cornerRadius).cgPath
+            tagBackgroundView.layer.shadowPath = UIBezierPath(roundedRect: tagBackgroundView.bounds, cornerRadius: tagCornerRadius).cgPath
             tagBackgroundView.layer.shadowOffset = shadowOffset
             tagBackgroundView.layer.shadowOpacity = shadowOpacity
             tagBackgroundView.layer.shadowRadius = shadowRadius
@@ -343,10 +343,10 @@ open class TagListView: UIView {
         tagView.highlightedBackgroundColor = tagHighlightedBackgroundColor
         tagView.selectedBackgroundColor = tagSelectedBackgroundColor
         tagView.titleLineBreakMode = tagLineBreakMode
-        tagView.cornerRadius = cornerRadius
-        tagView.borderWidth = borderWidth
-        tagView.borderColor = borderColor
-        tagView.selectedBorderColor = selectedBorderColor
+        tagView.cornerRadius = tagCornerRadius
+        tagView.borderWidth = tagBorderWidth
+        tagView.borderColor = tagBorderColor
+        tagView.selectedBorderColor = tagSelectedBorderColor
         tagView.paddingX = paddingX
         tagView.paddingY = paddingY
         tagView.textFont = textFont
